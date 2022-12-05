15d31ee2253a6efc7ac5adc54a86c875455e5ef8
kumar-tallapalli <77386924+kumar-tallapalli@users.noreply.github.com>
2022-12-05 16:31:48 +0530

    Update gtm_dynamic_load_script.js

--- gtm_dynamic_load_script.js
+++ gtm_dynamic_load_script.js
@@ -1,25 +1,16 @@
-//QA OneTrust Cookies Consent Notice start for infineon.com
+//PROD OneTrust Cookies Consent Notice start for infineon.com
 const OtAutoBlockScript = document.createElement('script');
-OtAutoBlockScript.src = 'https://cdn.cookielaw.org/consent/85f8227d-e015-4c62-ab96-ee1a08760fb3-test/OtAutoBlock.js';
+OtAutoBlockScript.src = 'https://cdn.cookielaw.org/consent/85f8227d-e015-4c62-ab96-ee1a08760fb3/OtAutoBlock.js';
 document.head.append(OtAutoBlockScript);
 
 const otSDKStubScript = document.createElement('script');
 otSDKStubScript.src = 'https://cdn.cookielaw.org/scripttemplates/otSDKStub.js';
 otSDKStubScript.setAttribute('data-document-language','true');
 otSDKStubScript.setAttribute('charset','UTF-8');
-otSDKStubScript.setAttribute('data-domain-script','85f8227d-e015-4c62-ab96-ee1a08760fb3-test');
+otSDKStubScript.setAttribute('data-domain-script','85f8227d-e015-4c62-ab96-ee1a08760fb3');
 document.head.append(otSDKStubScript);
-//QA OneTrust Cookies Consent Notice end for infineon.com
+//PROD OneTrust Cookies Consent Notice end for infineon.com
 
-
-// Google Tag Manager
-const GTMSCRIPT_ID = "GTM-N8N7WR8";
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer',GTMSCRIPT_ID);
-// End Google Tag Manager
 
 window.onload = function() {
 	// Google Tag Manager (noscript)
