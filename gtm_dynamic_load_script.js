b74ff25e909774abf27dbf76854103cb8b82b1c7
kumar-tallapalli <77386924+kumar-tallapalli@users.noreply.github.com>
2022-12-05 16:42:14 +0530

    Update gtm_dynamic_load_script.js

--- gtm_dynamic_load_script.js
+++ gtm_dynamic_load_script.js
@@ -11,6 +11,14 @@ otSDKStubScript.setAttribute('data-domain-script','85f8227d-e015-4c62-ab96-ee1a0
 document.head.append(otSDKStubScript);
 //PROD OneTrust Cookies Consent Notice end for infineon.com
 
+// Google Tag Manager
+const GTMSCRIPT_ID = "GTM-N8N7WR8";
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',GTMSCRIPT_ID);
+// End Google Tag Manager
 
 window.onload = function() {
 	// Google Tag Manager (noscript)
